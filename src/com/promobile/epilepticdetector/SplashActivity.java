143644bce6d4049add4f18041d8c065a483e3230
Larissa Ayres <larimayres@gmail.com>
2013-11-11 08:35:48 -0400

    Adicionando arquivos atualizados do projeto

---
+++ src/com/promobile/epilepticdetector/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.promobile.epilepticdetector;
+ 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import com.promobile.epilepticdetector.R;
+ 
+public class SplashActivity extends Activity {
+ 
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 6000;
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+ 
+        new Handler().postDelayed(new Runnable() {
+ 
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+ 
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+            	 Intent intent = new Intent();
+                 intent.setClass(SplashActivity.this, MainActivity.class);
+                 startActivity(intent);
+ 
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+ 
+}
