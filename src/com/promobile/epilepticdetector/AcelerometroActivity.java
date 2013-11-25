75f7e5d9f3f5ca31e63dfb750b1f22f379182990
erickrribeiro <erick.ribeiro.17@hotmail.com>
2013-11-25 14:33:18 -0400

    Adicionado log do Acelerômetro.

--- src/com/promobile/epilepticdetector/AcelerometroActivity.java
+++ src/com/promobile/epilepticdetector/AcelerometroActivity.java
@@ -1,13 +1,23 @@
 package com.promobile.epilepticdetector;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class AcelerometroActivity extends Activity implements SensorEventListener{
@@ -17,6 +27,7 @@ public class AcelerometroActivity extends Activity implements SensorEventListene
     private TextView textViewZ;
     private TextView textViewDetail;
      
+    Float x, y, z;
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
     
@@ -34,6 +45,15 @@ public class AcelerometroActivity extends Activity implements SensorEventListene
          
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        
+        Button btnCriarArquivo = (Button) findViewById(R.id.btnGerarArquivo);
+        btnCriarArquivo.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				CriaArquivosLog(x, y, z);
+			}
+		});
          
     }
        
@@ -55,25 +75,14 @@ public class AcelerometroActivity extends Activity implements SensorEventListene
  
     @Override
     public void onSensorChanged(SensorEvent event) {
-        Float x = event.values[0];
-        Float y = event.values[1];
-        Float z = event.values[2];
+        x = event.values[0];
+        y = event.values[1];
+        z = event.values[2];
         
         Log.d(TAG, "X: "+x.toString());
         Log.d(TAG, "Y: "+y.toString());
         Log.d(TAG, "Z: "+z.toString());
-        
-         /*
-        Os valores ocilam de -10 a 10.
-        Quanto maior o valor de X mais ele ta caindo para a esquerda - Positivo Esqueda 
-        Quanto menor o valor de X mais ele ta caindo para a direita  - Negativo Direita
-        Se o valor de  X for 0 então o celular ta em pé - Nem Direita Nem Esquerda
-        Se o valor de Y for 0 então o cel ta "deitado"
-         Se o valor de Y for negativo então ta de cabeça pra baixo, então quanto menor y mais ele ta inclinando pra ir pra baixo
-        Se o valor de Z for 0 então o dispositivo esta reto na horizontal.
-        Quanto maioro o valor de Z Mais ele esta inclinado para frente
-        Quanto menor o valor de Z Mais ele esta inclinado para traz.
-        */
+          
         textViewX.setText("Posição X: " + x.intValue() + " Float: " + x);
         textViewY.setText("Posição Y: " + y.intValue() + " Float: " + y);
         textViewZ.setText("Posição Z: " + z.intValue() + " Float: " + z);
@@ -88,6 +97,27 @@ public class AcelerometroActivity extends Activity implements SensorEventListene
                 textViewDetail.setText("Virando para ESQUERDA ");
             if(x < 0)  
                 textViewDetail.setText("Virando para DIREITA ");
-        }   
+        }
+        CriaArquivosLog(x, y, z);
+    }
+    public void CriaArquivosLog(Float x, Float y, Float z ){
+    	try {
+    		File arq = new File(Environment.getExternalStorageDirectory(), "logs.txt");
+			FileOutputStream escrever = new FileOutputStream(arq,true);
+			
+			escrever.write(x.toString().getBytes());
+			escrever.write(",".getBytes());
+			escrever.write(y.toString().getBytes());
+			escrever.write(",".getBytes());
+			escrever.write(z.toString().getBytes());
+			escrever.write(";".getBytes());
+			escrever.write("\n".getBytes());
+			escrever.flush();
+			escrever.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
