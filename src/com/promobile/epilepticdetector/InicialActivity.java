9bc1b6b78731255f973af24112038980fd43c52d
nosnilwar <nosnilwar@gmail.com>
2014-02-01 18:03:18 -0400

    MONITORAMENTO FUNCIONANDO COM A TELA DESLIGADA (SCREEN OFF) E CORRIGIDO BUG DA NOTIFICAÇÃO (AGORA FICA PERMANENTE).

--- src/com/promobile/epilepticdetector/InicialActivity.java
+++ src/com/promobile/epilepticdetector/InicialActivity.java
@@ -1,6 +1,7 @@
 package com.promobile.epilepticdetector;
 
 import android.app.Activity;
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -27,12 +28,18 @@ public class InicialActivity extends Activity {
 	String TAG = "MainActivity";
 	RadioButton start, stop;
 	int ID = 101;
-	
+
+	private PendingIntent pendingIntent;
+    private AlarmManager alarmManager;
+	private final int TIMER_MONITORAMENTO_HEURISTICA = 100; // EM milisegundos...
+
 	 @Override
      public void onCreate(Bundle savedInstanceState)
      {
          super.onCreate(savedInstanceState);
          setContentView(R.layout.activity_inicial);
+         
+         alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
          
          start = (RadioButton) findViewById(R.id.btnBackgroundOn); //botao ligar
          stop = (RadioButton) findViewById(R.id.btnBackgroundOff); //botao desligar
@@ -62,53 +69,60 @@ public class InicialActivity extends Activity {
         			startActivity(intent);
         		}
         	});
-}
-
-private void toggleService(){
-   Intent intent=new Intent(getApplicationContext(), EpilepsyHeuristicService.class);
-   // Try to stop the service if it is already running
-   intent.addCategory(EpilepsyHeuristicService.TAG);
-   if(!stopService(intent)){
-       startService(intent);
-   }
-}
-
-public void startSensing(){
-	showNotification();
-	toggleService();
-	//this.finish();
-}
-
-private void stopSensing(){
-	Intent intent = new Intent(getApplicationContext(), EpilepsyHeuristicService.class);
-	intent.addCategory(EpilepsyHeuristicService.TAG);
-	stopService(intent);
-	mNotifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-	mNotifyManager.cancel(ID);
+	}
 	
-}
+	private void toggleService(){
+		Context context = getApplicationContext();
+		Intent intent = new Intent(context, EpilepsyHeuristicService.class);
+		// Try to stop the service if it is already running
+		intent.addCategory(EpilepsyHeuristicService.TAG);
 
-public void showNotification(){
-	mBuilder = new NotificationCompat.Builder(this);
-	mBuilder.setContentTitle("Epileptic Detector")
-	    .setContentText("Monitorando possível ataque")
-	    .setSmallIcon(R.drawable.ic_launcher);
-	Intent resultIntent = new Intent(this, MainActivity.class);
-
+		AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+		pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+		alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), TIMER_MONITORAMENTO_HEURISTICA, pendingIntent);
+   
+		if(!stopService(intent)){
+			startService(intent);
+		}
+	}
 	
-	TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-	stackBuilder.addParentStack(MainActivity.class);
-	stackBuilder.addNextIntent(resultIntent);
-	PendingIntent resultPendingIntent =
-	        stackBuilder.getPendingIntent(
-	            0,
-	            PendingIntent.FLAG_ONE_SHOT
-	        );
-	mBuilder.setOngoing(true);
-	mBuilder.setContentIntent(resultPendingIntent);
-	mNotifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-	mNotifyManager.notify(ID, mBuilder.build());	
-         
+	public void startSensing(){
+		showNotification();
+		toggleService();
+		//this.finish();
+	}
+	
+	private void stopSensing(){
+		Intent intent = new Intent(getApplicationContext(), EpilepsyHeuristicService.class);
+		intent.addCategory(EpilepsyHeuristicService.TAG);
+		stopService(intent);
+		mNotifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		mNotifyManager.cancel(ID);
+		
+		Context context = getApplicationContext();
+		AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+		alarmManager.cancel(pendingIntent);
+	}
+	
+	public void showNotification(){
+		mBuilder = new NotificationCompat.Builder(this);
+		mBuilder.setContentTitle("Epileptic Detector")
+		    .setContentText("Monitorando possível ataque")
+		    .setSmallIcon(R.drawable.ic_launcher);
+		Intent resultIntent = new Intent(this, MainActivity.class);
+	
+		
+		TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+		stackBuilder.addParentStack(MainActivity.class);
+		stackBuilder.addNextIntent(resultIntent);
+		PendingIntent resultPendingIntent =
+		        stackBuilder.getPendingIntent(
+		            0,
+		            PendingIntent.FLAG_ONE_SHOT
+		        );
+		mBuilder.setOngoing(true);
+		mBuilder.setContentIntent(resultPendingIntent);
+		mNotifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		mNotifyManager.notify(ID, mBuilder.build());	
      }
-
 }
